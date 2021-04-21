621857755b74b89cfbfca3af3b81a6ac68708305
Povilas <p.bikmanas@gmail.com>
2021-04-21 23:01:30 +0300

    #4 atlikta

--- js/main.js
+++ js/main.js
@@ -1,4 +1,9 @@
-import {months } from './data/months.js';
+import { months } from './components/months.js';
+import { balance } from './components/balance.js';
+import { account } from './data.js';
 
 
-console.log(months);
+months('.table-content', account);
+months;
+account;
+balance('.table-footer', account);
