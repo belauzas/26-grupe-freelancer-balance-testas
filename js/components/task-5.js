7c7eafe7e8397cbd01b6318fbed38338f541d726
Uugne <ugne.kurkyte@gmail.com>
2021-04-22 18:35:11 +0300

    task 5

---
+++ js/components/task-5.js
@@ -0,0 +1,30 @@
+function suma(selector, account) {
+    
+        const DOM = document.querySelector(selector);
+
+        let incomeSum = 0;
+        let expenseSum = 0;
+
+        for (let i = 0; i < account.length; i++) {
+            let {income} = account[i];
+            incomeSum += income;
+            let {expense} = account[i];
+            expenseSum += expense;
+
+        }
+    
+        let HTML = '';
+    
+                    HTML += `   <div class="cell"></div>
+                                <div class="cell"></div>
+                                <div class="cell">${incomeSum} Eur</div>
+                                <div class="cell">${expenseSum} Eur</div>
+                                <div class="cell">${incomeSum - expenseSum}</div>
+                              </div>`
+
+                
+                    DOM.innerHTML += HTML;
+        }
+    
+        
+export { suma }
--- js/main.js
+++ js/main.js
@@ -1,5 +1,7 @@
 import { month } from './components/menesiai.js'; 
 import { account } from './data.js';
+import { suma } from './components/task-5.js';
 
 
 month('.table-content', account);
+suma('.table-footer', account);
