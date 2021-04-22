c60538db82302d7fe9ae66cdb97acadc90741517
Kerbelyte <80693721+Kerbelyte@users.noreply.github.com>
2021-04-22 12:40:58 +0300

    finnished task 4 and 5

--- js/components/table.js
+++ js/components/table.js
@@ -1,8 +1,6 @@
 function table(selector, data) {
     const DOM = document.querySelector(selector);
     let HTML = '';
-
-
     const months = [
         'Sausis',
         'Vasaris',
@@ -17,29 +15,39 @@ function table(selector, data) {
         'Lapkritis',
         'Gruodis'
     ];
+    let sumIncomes = 0;
+    let sumExpenses = 0;
+    let sumBalanses = 0;
 
     const sortedDataByMonth = data.sort((a, b) => a.month > b.month ? 1 : -1);
     for (let i = 0; i < data.length; i++) {
         const row = sortedDataByMonth[i];
-        
-        let month = months[i];
-
-        let income = row.income === undefined ? 0 : row.income
-        let expense = row.expense === undefined ? 0 :row.expense
-        let balance = income - expense;
-        let list = row.month;
+        const month = months[row.month - 1];
+        const income = row.income === undefined ? 0 : row.income
+        const expense = row.expense === undefined ? 0 : row.expense
+        const balance = income - expense;
 
         HTML += `<div class="table-row">
-                    <div class="cell">${list}</div>
+                    <div class="cell">${i + 1}</div>
                     <div class="cell">${month}</div>
                     <div class="cell">${income}</div>
                     <div class="cell">${expense}</div>
                     <div class="cell">${balance}</div>
                 </div>`
-                
 
+        sumIncomes += income;
+        sumExpenses += expense;
+        sumBalanses += balance;
     }
     DOM.innerHTML += HTML;
+
+    const incomeDOM = document.getElementById('income');
+        incomeDOM.innerText = `${sumIncomes} Eur`;
+    const expenseDOM = document.getElementById('expense');
+        expenseDOM.innerText = `${sumExpenses} Eur`;
+    const balanceDOM = document.getElementById('balance');
+        balanceDOM.innerText = `${sumBalanses} Eur`;
+
 
 }
 
