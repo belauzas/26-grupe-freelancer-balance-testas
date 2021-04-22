1f1cf05925beb5162cb526d37bd241aaf3e377c9
Mantas <mantasu11@gmail.com>
2021-04-22 12:55:58 +0300

    table-footer dalis uzpildyta

--- js/main.js
+++ js/main.js
@@ -1,8 +1,11 @@
 import { account } from './data.js';
 import { months } from './data.js';
 import { yearsBalance } from './components/yearsBalance.js';
+import { footerSums } from './components/footerSums.js';
+
 
 yearsBalance('.table-content', account, months);
+footerSums ('.table-footer', account);
 
 
 
