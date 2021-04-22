885f57f03e7bfe48f18874a19d9213446a3b4ade
Kerbelyte <80693721+Kerbelyte@users.noreply.github.com>
2021-04-22 10:03:05 +0300

    corrected months array

--- js/main.js
+++ js/main.js
@@ -1,14 +1,2 @@
-const months = [
-    'Sausis',
-    'Vasaris',
-    'Kovas',
-    'Balandis',
-    'Gegužė',
-    'Birželis',
-    'Liepa',
-    'Rugpjūtis',
-    'Rugsėjis',
-    'Spalis',
-    'Lapkritis',
-    'Gruodis'
-];
+import { months } from './components/months';
+
