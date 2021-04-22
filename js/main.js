04a3d2954a6ab821d691ce5a98d1af6d68372f9b
Aldas <aldevinas.kasputis@gmail.com>
2021-04-22 12:09:36 +0300

    #5 year total

--- js/main.js
+++ js/main.js
@@ -1,6 +1,8 @@
 import { months } from './months.js';
 import { yearBalance } from './yearBalance.js';
+import { yearTotal } from './yearTotal.js';
 import { account } from './data.js';
 
 console.log(months); 
 yearBalance('.table-content', account, months); 
+yearTotal('.table-footer', account);
