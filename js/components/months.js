2abd1892823e67fdd8549d7f15ee261b0e273b62
Povilas <p.bikmanas@gmail.com>
2021-04-23 17:39:27 +0300

    Balance part updated

--- js/components/months.js
+++ js/components/months.js
@@ -2,13 +2,14 @@ function months(selector, data, months){
     const DOM = document.querySelector(selector);
     let HTML = '';
     let num = 0;
+    let balance = 0;
     
     let sorted = data.sort((a, b) => a.month - b.month);
     for (let i = 0; i < data.length; i++){
         const row = data[i];
         row.month = months[i];
         num++;
-        
+                
         if(row.income === undefined){
             row.income = 0;
         }
@@ -16,7 +17,8 @@ function months(selector, data, months){
             row.expense = 0;
         }
        
-        const balance = row.income - row.expense;
+        balance += row.income ? row.income : 0;
+        balance -= row.expense ? row.expense : 0;
 
         HTML += `<div class="table-row">
                     <div class="cell">${num}</div>
